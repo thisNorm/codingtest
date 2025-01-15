181dc56bfce4f3bb4134fcf611f5be134b855918
theNorm <kisook2557@gmail.com>
2025-01-15 14:23:55 +0900

    완성된 코드

--- 1월 2주차/practice0114.java
+++ 1월 2주차/practice0114.java
@@ -3,11 +3,11 @@ class Solution {
         boolean answer = true;
         int sum = 0;
         String str = String.valueOf(x);
-        char[] Array = toCharArray(x);
+        char[] Array = str.toCharArray();
         
         if ( 1 <= x && x <= 10000 ) {
             for ( int i = 0; i < str.length(); i ++ ) {
-                sum += (int)Array[i];
+                sum += Character.getNumericValue(Array[i]);
             }
 
             if ( x % sum == 0 ) {
@@ -18,11 +18,16 @@ class Solution {
                 return false;
             }
         }
-    }
-}
 
-public void main(strings[] args) {
-    int x = 12;
+        else {
+            return false;
+        }
+    }
+
+
+public static void main(String[] args) {
+    int x = 10;
     Solution solution = new Solution();
     System.out.println(solution.solution(x));
+    }
 }
