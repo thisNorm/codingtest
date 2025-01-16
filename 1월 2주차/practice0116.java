f409b3e290d7139c1f267a03052b921af8d51275
theNorm <kisook2557@gmail.com>
2025-01-16 18:32:26 +0900

    두 정수 사이의 합 최종

--- 1월 2주차/practice0116.java
+++ 1월 2주차/practice0116.java
@@ -1,30 +1,25 @@
 class practice0116 {  
     public long solution(int a, int b) {  
         long answer = 0;  
+    
         // a, b 값 범위 검사  
         if (-10000000 <= a && a <= 10000000  
                 && -10000000 <= b && b <= 10000000) {  
-            // a > b  
-            if (a > b) {  
-                for (int i = b; i <= a; i++) {  
-                    answer += i;  
-                }  
-            }  
-            // a < b  
-            else if (a < b) {  
-                for (int i = a; i <= b; i++) {  
-                    answer += i;  
-                }  
-            }  
-            // a == b  
-            else {  
-                answer = a;  
+            
+            // Math 클래스를 사용해 a와 b의 대소 관계를 간단히 처리  
+            int start = Math.min(a, b);  
+            int end = Math.max(a, b);  
+    
+            for (int i = start; i <= end; i++) {  
+                answer += i;  
             }  
         }  
+    
         return answer;  
     }  
+    
     public static void main(String[] args) {  
         practice0116 solution = new practice0116();  
         System.out.println(solution.solution(3, 5));  // Expected output: 12  
-    }  
+    }
 }
