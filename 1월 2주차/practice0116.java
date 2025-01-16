301357b3a9026afcdec852261db35ed789abaf7e
theNorm <kisook2557@gmail.com>
2025-01-16 18:30:59 +0900

    두 정수 사이의 합 1차 코딩

--- 1월 2주차/practice0116.java
+++ 1월 2주차/practice0116.java
@@ -1,3 +1,30 @@
-public class practice0116 {
-    
+class practice0116 {  
+    public long solution(int a, int b) {  
+        long answer = 0;  
+        // a, b 값 범위 검사  
+        if (-10000000 <= a && a <= 10000000  
+                && -10000000 <= b && b <= 10000000) {  
+            // a > b  
+            if (a > b) {  
+                for (int i = b; i <= a; i++) {  
+                    answer += i;  
+                }  
+            }  
+            // a < b  
+            else if (a < b) {  
+                for (int i = a; i <= b; i++) {  
+                    answer += i;  
+                }  
+            }  
+            // a == b  
+            else {  
+                answer = a;  
+            }  
+        }  
+        return answer;  
+    }  
+    public static void main(String[] args) {  
+        practice0116 solution = new practice0116();  
+        System.out.println(solution.solution(3, 5));  // Expected output: 12  
+    }  
 }
